006ce46c8461b581408bbdc17b38136a0a1a7e00
George Cope <G.Cope13@gmail.com>
2021-01-23 16:02:53 +0000

    Made some minor changes to HTML & CSS. Created a function that takes items from the array in local storage and appends the text to dynamically generated buttons in the <aside> element

--- WeatherDashboard.js
+++ WeatherDashboard.js
@@ -12,7 +12,7 @@ var displayCurrent = function(serverResponse){
     $("#temperature").append("&deg;C");
     $("#humidity").text("Relative Humidity: " + humidity + "%");
     $("#windSpeed").text("Wind Speed: " + windSpeed.toFixed(2) + "kmph");
-    $("#uvIndex").text("UV Index: " + uvIndex);
+    $("#uvIndexBtn").text(uvIndex);
 
     var currIcon = serverResponse.current.weather[0].icon;
     var currIconURL = "http://openweathermap.org/img/wn/" + currIcon + ".png"
@@ -22,6 +22,14 @@ var displayCurrent = function(serverResponse){
     var humanDate = dateObject.toLocaleString("en-GB", {day: "numeric", month: "numeric", year: "numeric"});
     $("#cityName").append(" - " + humanDate);
     $("#weatherIcon").attr("src", currIconURL);
+
+    if(uvIndex < 3){
+        $("#uvIndexBtn").attr("class", "btn btn-primary btn-sm");
+    } else if(uvIndex < 5){
+        $("#uvIndexBtn").attr("class", "btn btn-success btn-sm");
+    } else{
+        $("#uvIndexBtn").attr("class", "btn btn-danger btn-sm");
+    }
 
 }
 
@@ -53,25 +61,22 @@ var saveSearch = function(){
     searchHistory.push(newSearch);
     console.log(searchHistory);
     localStorage.setItem("History", JSON.stringify(searchHistory));
-    
-    for(i = 0; i < searchHistory.length; i++){
-        $("#searchHistory").append("<button id='history" + [i] + "'>");
-        $("#history" + [i]).append(searchHistory + [i]);
+}
+
+var writeSearch = function(){
+    saveSearch();
+    var searchHistory = JSON.parse(localStorage.getItem("History"));
+    $("#searchHistory").empty();
+    for(var i = 0; i < searchHistory.length; i++){
+        $("#searchHistory").append("<button class='searchHistory' id='item" + [i] + "'>");
+        $("#item" + [i]).text(searchHistory[i]);
     }
 }
 
-$("#searchBtn").on("click", function(event){
-    event.preventDefault();
-
-    saveSearch();
-
+var ajaxCall = function(queryURL){
     // These variables takes the user's input from the search bar and add them to the queryURL 
     var q = $("#searchQuery").val();
     var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + q + "&appid=c7fb2f80502825ecbe90a5fece0767e4";
-
-    // Here, the DOM elements selected are being displayed
-    $("#currForecast").css("display", "block");
-    $("#futureForecast").css("display", "block");
 
     // The first AJAX call is initialised here to get the latitude and longitude variables we need in order to make the second AJAX call
     $.ajax({
@@ -99,14 +104,24 @@ $("#searchBtn").on("click", function(event){
             display5Day(serverResponse);
         });
     });
-    
-    
+}
+
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+
+    writeSearch();
+
+    ajaxCall();
+
+    // Here, the DOM elements selected are being displayed
+    $("#currForecast").css("display", "block");
+    $("#futureForecast").css("display", "block");
 })
 
 
 
 // TO DO:
-// GENERATE WEATHER ICONS FOR CURRENT FORECAST & 5-DAY FORECAST
-// CREATE SOME KIND OF IF/ELSE STATEMENT FOR THE UV INDEX. THIS SHOULD COLOUR THE VALUE DEPENDING ON THE SEVERITY OF THE UV INDEX
-// SAVE EACH WEATHER SEARCH TO LOCAL STORAGE, APPEND THE VALUES TO BUTTONS IN #searchHistory ELEMENT
-// HAVE EACH BUTTON IN #searchHistory SEARCH FOR THAT BUTTON'S VALUE
+// GENERATE WEATHER ICONS FOR CURRENT FORECAST & 5-DAY FORECAST                                                                            - DONE
+// CREATE SOME KIND OF IF/ELSE STATEMENT FOR THE UV INDEX. THIS SHOULD COLOUR THE VALUE DEPENDING ON THE SEVERITY OF THE UV INDEX          - 
+// SAVE EACH WEATHER SEARCH TO LOCAL STORAGE, APPEND THE VALUES TO BUTTONS IN #searchHistory ELEMENT                                       - 
+// HAVE EACH BUTTON IN #searchHistory SEARCH FOR THAT BUTTON'S VALUE                                                                       - 
